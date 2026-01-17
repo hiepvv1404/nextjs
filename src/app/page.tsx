1ad8b517ab98cf17cfcae25078698f8fbde5d666
HiepVV <vuvanhiep1404@gmail.com>
2026-01-17 21:21:14 +0700

    useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,31 @@ import Link from 'next/link';
 import y from '@/styles/hoidanit.module.css';
 import AppTable from '@/components/app.table';
 import { useEffect } from 'react';
+import useSWR from 'swr';
 
 export default function Home() {
-  const res = fetch('http://localhost:8000/blogs');
-  console.log(res);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/blogs');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const { data, error, isLoading } = useSWR('http://localhost:8000/blogs', fetcher, {
+    // cấu hình để cache data tránh load lại nhiều lần khi chuyển trang qua lại dùng cho data ít thay đổi
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:8000/blogs');
+  //       const data = await response.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
